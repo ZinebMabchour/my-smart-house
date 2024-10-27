7a267a20ea830eb1682fcad8bd37244b2be75b3f
ZinebMabchour <zeynabmabshour@gmail.com>
2024-10-27 23:39:36 +0100

    SCRUM-1 updated AppareilController.java

--- src/main/java/com/example/smarthouse/Controller/AppareilController.java
+++ src/main/java/com/example/smarthouse/Controller/AppareilController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.annotation.Target;
-import java.util.List;
+import java.util.*;
 import java.util.Optional;
 
 @RestController
